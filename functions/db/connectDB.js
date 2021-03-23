cb673c167373d128034cf77c04ccfc324f3a9faa
Victor Ajibade <ajibadevic@gmail.com>
2021-03-23 10:05:51 +0100

    Adjust DB connection options

--- functions/db/connectDB.js
+++ functions/db/connectDB.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const uri = process.env.MONGO_URI;
 
 const connectDB = async () => {
+  // check if we have a connection to the database or if it's currently
+  // connecting or disconnecting (readyState 1, 2 and 3)
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    throw new Error("No uri found");
+  }
   try {
     const conn = await mongoose.connect(uri, {
       useNewUrlParser: true,
@@ -10,6 +18,7 @@ const connectDB = async () => {
       useFindAndModify: false,
     });
     console.log(`CONNECTED TO ${conn.connection.db.databaseName} on: ${conn.connection.host}`);
+    return conn;
   } catch (err) {
     console.error(err);
     process.exit(1);
